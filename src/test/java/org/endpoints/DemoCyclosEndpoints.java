c5f07a54991417a0df7f336f6a480b527a72d0d6
Kabilash2327 <kabileshpanneerselvam@gmail.com>
2023-05-29 09:23:17 +0530

    26.05 tasks updated

--- src/test/java/org/endpoints/DemoCyclosEndpoints.java
+++ src/test/java/org/endpoints/DemoCyclosEndpoints.java
@@ -9,9 +9,19 @@ package org.endpoints;
 
 public interface DemoCyclosEndpoints {
 	
-	public final static String BASEURLDEMOCYCLOS = "https://demo.cyclos.org/api/self";
+	public final static String BASEURLDEMOCYCLOS = "https://demo.cyclos.org/api";
 
-	public final static String GET_GETACCOUNT = "/accounts";
+	public final static String GET_GETACCOUNT = "/self/accounts";
 	
-	public final static String GET_LISTOFACCOUNTS = "/accounts/list-data";
+	public final static String GET_LISTOFACCOUNTS = "/self/accounts/list-data";
+	
+	public final static String GET_ACCOUNTTYPE = "/self/accounts/memberaccount";
+	
+	public final static String GET_ADDRESS_LIST_DATA = "/self/addresses/list-data";
+	
+	public final static String GET_AUTH_SESSION = "/auth/session";
+	
+	public final static String GET_UI_DATA = "/ui/data-for-ui";
+		
+	
 }
--- src/test/java/org/practise_API/DemoCyclos.java
+++ src/test/java/org/practise_API/DemoCyclos.java
@@ -1,8 +1,8 @@
 package org.practise_API;
 
 import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
-
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import org.endpoints.DemoCyclosEndpoints;
 import org.testng.annotations.Test;
 
@@ -10,11 +10,10 @@ public class DemoCyclos implements DemoCyclosEndpoints {
 
 	public String queryPathforTC02 = "?fields=status";
 
-	
 	/**
 	 * 25-5
 	 */
-	
+
 	@Test
 	public void tc01_get_accounts() {
 
@@ -27,7 +26,7 @@ public class DemoCyclos implements DemoCyclosEndpoints {
 	/**
 	 * 25-5
 	 */
-	
+
 	@Test
 	public void tc02_get_accounts_passing_query() {
 
@@ -36,7 +35,7 @@ public class DemoCyclos implements DemoCyclosEndpoints {
 				.body("status[0].balance", equalTo("11380.99")).log().all();
 
 	}
-	
+
 	/**
 	 * 25-5
 	 */
@@ -48,4 +47,53 @@ public class DemoCyclos implements DemoCyclosEndpoints {
 				.body("user.display", equalTo("Kabilesh")).log().all();
 
 	}
+
+	/**
+	 * 26-5
+	 */
+
+	@Test
+	public void tc04_get_account_accountType() {
+
+		given().header("accept", "application/json").header("Authorization", "Basic a2FiaToyMzI3").when()
+				.get(BASEURLDEMOCYCLOS + GET_ACCOUNTTYPE).then().statusCode(200)
+				.body("type.name", equalTo("Member account")).log().all();
+	}
+
+	/**
+	 * 26-05
+	 */
+	@Test
+	public void tc05_get_address_list_data() {
+
+		given().header("accept", "application/json").header("Authorization", "Basic a2FiaToyMzI3")
+		       .when().get(BASEURLDEMOCYCLOS + GET_ADDRESS_LIST_DATA)
+			   .then().statusCode(200).body("enablePrivacy", is(true)).log().all();
+
+	}
+
+	/**
+	 * 26-05
+	 */
+	@Test	
+	public void tc06_get_auth_session() {
+		given().header("accept", "application/json").header("Authorization", "Basic a2FiaToyMzI3").when()
+				.get(BASEURLDEMOCYCLOS + GET_AUTH_SESSION).
+				then().statusCode(204).log().all();
+
+	}
+
+	/**
+	 * 26-05
+	 */
+	@Test
+	public void tc07_get_ui_data() {
+		given().header("accept", "application/json").header("Authorization", "Basic a2FiaToyMzI3").when()
+				.get(BASEURLDEMOCYCLOS + GET_UI_DATA).then().statusCode(200)
+				.body("auth.user.id", is("7762070814192302143")).log().all();
+		
+	}
+	
+	
+
 }
